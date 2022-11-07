0db350d4408eef29acbb745ff5e39100460760d9
MathieuH <huguet.mathieu@gmail.com>
2022-11-07 20:59:33 +0100

    side nav hamburger + animation ok

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
+//On click side nav
+const sideNavHam = document.querySelector(".ham-side-container");
+let sideNavOpen = false;
+//Top side nav animation
+// sideNavHam.addEventListener("click", () => {
+//   if (!sideNavOpen) {
+//     sideNavHam.classList.add("hamNavOpen");
+//     sideNavOpen = true;
+//   } else {
+//     sideNavHam.classList.add("hamNavOpen");
+//     sideNavOpen = false;
+//   }
+// });
+//Hamburger selector
 const menuHam = document.querySelector(".hamburger");
 let hamOpen = false;
-
+//Hamburger animation
 menuHam.addEventListener("click", () => {
-  if (!hamOpen) {
+  if (!hamOpen && !sideNavOpen) {
     menuHam.classList.add("open-menu");
     hamOpen = true;
+    sideNavHam.classList.add("hamNavOpen");
+    // sideNavHam.style.transform("translateY(0)");
+    sideNavOpen = true;
   } else {
     menuHam.classList.remove("open-menu");
     hamOpen = false;
+    sideNavHam.classList.remove("hamNavOpen");
+    sideNavOpen = false;
   }
 });
