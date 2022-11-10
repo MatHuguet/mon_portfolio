7d88a547be7fce07410df4c85458b27fe3471a91
MathieuH <huguet.mathieu@gmail.com>
2022-11-10 18:39:44 +0100

    modif top Side Nav for small screen

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
 //On click side nav
 const sideNavHam = document.querySelector(".ham-side-container");
 let sideNavOpen = false;
-//Top side nav animation
-// sideNavHam.addEventListener("click", () => {
-//   if (!sideNavOpen) {
-//     sideNavHam.classList.add("hamNavOpen");
-//     sideNavOpen = true;
-//   } else {
-//     sideNavHam.classList.add("hamNavOpen");
-//     sideNavOpen = false;
-//   }
-// });
 //Hamburger selector
 const menuHam = document.querySelector(".hamburger");
 let hamOpen = false;
+//Définition items navS
+const listNavItemS = document.querySelector(".nav-phone-tab");
+
 //Hamburger animation
 menuHam.addEventListener("click", () => {
   if (!hamOpen && !sideNavOpen) {
     menuHam.classList.add("open-menu");
     hamOpen = true;
     sideNavHam.classList.add("hamNavOpen");
-    // sideNavHam.style.transform("translateY(0)");
     sideNavOpen = true;
   } else {
     menuHam.classList.remove("open-menu");
