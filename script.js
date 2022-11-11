e527d3eab1cf7b07abf0e2ca2c7e8128cc4ee441
MathieuH <huguet.mathieu@gmail.com>
2022-11-11 17:43:22 +0100

    finish phone design. Added first content

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
-//On click side nav
 const sideNavHam = document.querySelector(".ham-side-container");
-let sideNavOpen = false;
-//Hamburger selector
 const menuHam = document.querySelector(".hamburger");
-let hamOpen = false;
-//Définition items navS
-const listNavItemS = document.querySelector(".nav-phone-tab");
+
+menuHam.addEventListener("click", () => {
+  menuHam.classList.toggle("open-menu");
+  sideNavHam.classList.toggle("hamNavOpen");
+});
+
+//-------------LA CASSE-------------------
+
+//On click side nav
+// const sideNavHam = document.querySelector(".ham-side-container");
+// let sideNavOpen = false;
+//Hamburger selector
+// const menuHam = document.querySelector(".hamburger");
+// let hamOpen = false;
+//définition menu top slider
+// const slideMenuItems = document.querySelectorAll("ham-side-menu");
+// let slideMenuItemsOpen = false;
 
 //Hamburger animation
-menuHam.addEventListener("click", () => {
-  if (!hamOpen && !sideNavOpen) {
-    menuHam.classList.add("open-menu");
-    hamOpen = true;
-    sideNavHam.classList.add("hamNavOpen");
-    sideNavOpen = true;
-  } else {
-    menuHam.classList.remove("open-menu");
-    hamOpen = false;
-    sideNavHam.classList.remove("hamNavOpen");
-    sideNavOpen = false;
-  }
-});
+// menuHam.addEventListener("click", () => {
+//   if (!hamOpen && !sideNavOpen && !slideMenuItemsOpen) {
+//     menuHam.classList.add("open-menu");
+//     hamOpen = true;
+//     sideNavHam.classList.add("hamNavOpen");
+//     sideNavOpen = true;
+//   } else {
+//     menuHam.classList.remove("open-menu");
+//     hamOpen = false;
+//     sideNavHam.classList.remove("hamNavOpen");
+//     sideNavOpen = false;
+//   }
+// });
